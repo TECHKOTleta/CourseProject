fc37a50898011c98c57102006b32c37f121eca37
Sergey_Smirnov <sergey.smirnov.200@mail.ru>
2024-10-16 14:04:08 +0300

    Завершена работа над блоком календаря. Создан визуал блока выбора билета по возрасту

--- ProjectBeforeBD/ProjectBeforeBD/obj/Debug/net8.0-windows/App.g.i.cs
+++ ProjectBeforeBD/ProjectBeforeBD/obj/Debug/net8.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace ProjectBeforeBD {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace ProjectBeforeBD {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
         public static void Main() {
             ProjectBeforeBD.App app = new ProjectBeforeBD.App();
             app.InitializeComponent();
