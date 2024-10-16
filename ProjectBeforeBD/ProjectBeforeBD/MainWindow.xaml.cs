9bb99f23150dcc1cac4a1e253ef377b59b2dc623
Сергей Смирнов <sergey.smirnov.200@mail.ru>
2024-10-16 06:29:14 +0300

    Создано автозаполнение календаря на текущий месяц

--- ProjectBeforeBD/ProjectBeforeBD/MainWindow.xaml.cs
+++ ProjectBeforeBD/ProjectBeforeBD/MainWindow.xaml.cs
@@ -16,9 +16,48 @@ namespace ProjectBeforeBD
     /// </summary>
     public partial class MainWindow : Window
     {
+        public void Preparations()
+        {
+            DateGrid.Visibility = Visibility.Hidden;
+            CalendarSettings();
+        }
+
+        public void CalendarSettings()
+        {
+            DateTime now = DateTime.Now;
+            DateTime startOfTheMonth = new DateTime(now.Year, now.Month, 1);
+            int days = DateTime.DaysInMonth(now.Year, now.Month);
+            int dayOfTheWeek = Convert.ToInt32(startOfTheMonth.DayOfWeek.ToString("d"))+1;
+            
+            
+            for (int i = 1; i < dayOfTheWeek; i++)
+            {
+                Rectangle blank = new Rectangle();
+                blank.Width = 22.5;
+                blank.Height = 18.755;
+                CalenPanel.Children.Add(blank);
+            }
+
+            for (int i = 1; i < days; i++)
+            {
+                Button TextDate = new Button();
+                TextDate.Width = 22.5;
+                TextDate.Height = 18.755;
+                TextDate.Content = i.ToString();
+                CalenPanel.Children.Add(TextDate);
+            }
+        }
         public MainWindow()
         {
             InitializeComponent();
+            Preparations();
+        }
+
+
+        //---------------------------------------------------НАЧАЛО ОПИСАНИЯ ЭЛЕМЕНТОВ---------------------------------------------
+        private void DateBut_Click(object sender, RoutedEventArgs e)
+        {
+            DateGrid.Visibility = (DateGrid.Visibility == Visibility.Visible) ? Visibility.Hidden : Visibility.Visible;
         }
     }
 }
